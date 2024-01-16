89c963753c6a6ef855b7b8a947cca24ae41b8988
JuanSoftware <juan.contato0101@gmail.com>
2024-01-15 20:29:23 -0400

    ai

--- cadastro_alunos/routes.js
+++ cadastro_alunos/routes.js
@@ -90,8 +90,96 @@ router.get("/aulas", (req,res) =>{
     });
 })
 
-router.put('/aulas/:id', )
+router.put('/aulas/:id', (req, res) =>{
+    const{id} = req.params;
 
+    const{nome_aula, descricao} = req.body;
+    
+    const query = `UPDATE aulas SET nome_aula='${nome_aula}', descricao= '${descricao}' WHERE id_aula=${id}`;
+
+    db.query(query, (err, result) =>{
+        if(err){
+            res.status(500).send(`Erro ao atualizar aula: ${JSON.stringify(err)}`);
+        }else{
+            res.send('Aula atualizado com sucesso');
+        }
+    });
+}) 
+
+router.delete('/aulas/:id', (req,res) =>{
+    
+    const{id} = req.params;
+
+    
+    const query = `DELETE FROM aulas WHERE id_aula=${id}`;
+
+    db.query(query, (err, result) =>{
+        if(err){
+            res.status(500).send('Erro ao excluir aula');
+        }else{
+            res.send('Aula excluido com sucesso');
+        }
+    });
+});
+
+
+ // Ednpoint para criar inscrição em aulas
+ router.post('/inscricao', (req,res) => {
+    const {id_aula, id_aluno} = req.body
+    const query = `INSERT INTO alunos_aulas (id_aula, id_aluno) VALUES (${id_aula}, ${id_aluno})`
+
+    db.query(query,(err,result) => {
+      if(err){
+        res.status(500).send(`Erro ao inscrever-se`);
+      }else {
+        res.send(`Inscrição realizada com sucesso! :)`)
+      }
+    })
+  })
+
+  // Endpoint para verificar a inscrição 
+
+  router.get('/inscricao', (req,res)=> {
+    const query = `SELECT * FROM alunos_aulas`;
+
+    db.query(query, (err,result)=>{
+      if(err){
+        res.status(500).send("Erro ao localizar inscrição")
+      } else{
+        res.json(result);
+      }
+    })
+  })
+
+  //Endpoint pra retornar todas as aulas de um aluno específico
+  router.get('/inscricao/:id', (req, res)=>{
+    const {id} = req.params
+    const query = `SELECT * FROM alunos_aulas WHERE id_aluno = ${id}`
+
+    db.query(query, (err, result)=>{
+      if(err){
+        res.status(500).send(`Erro ao retornar aluno: ${JSON.stringify(err)}`)
+      } else {
+        res.json(result)
+      }
+    })
+  })
+  
+  //ENDPOINT atualização alunos e aula
+  router.put('/inscricao/:id', (req, res) =>{
+    const {id} = req.params;
+    const{id_aula, id_aluno} = req.body;
+
+    const query = `UPDATE alunos_aulas SET id_aluno=${id_aluno}, id_aula =${id_aula} WHERE id_aluno =${id} `
+
+    db.query(query, (err, result)=>{
+      if(err){
+        res.status(500).send(`Erro ao atualizar aluno: ${JSON.stringify(err)}`)
+      } else {
+        res.json(result)
+      }
+    })
+})
 
 
 module.exports = router;
